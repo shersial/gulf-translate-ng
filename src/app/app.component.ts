89d40daa3fb4d8466b8902a6b72a80ce6256f4f9
Muhammed Zahid <shersials@gmail.com>
2021-07-01 12:49:18 +0400

    add translate
    add translate

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+
+import {_} from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
+
+import {TranslateService} from '@ngx-translate/core';
+
+import defaultLanguage from './../assets/i18n/en.json';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+    fromCode = _('demo.text-in-code');
+
+    constructor(private translate: TranslateService) {
+        translate.setTranslation('en', defaultLanguage);
+        translate.setDefaultLang('en');
+    }
+
+    useLanguage(language: string) {
+        this.translate.use(language);
+    }
+}
